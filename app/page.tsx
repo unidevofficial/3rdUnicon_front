d9947197a7b8d57dca29bc45c9371762872f5e81
ore-o <cyhs9120@naver.com>
2025-10-29 12:18:49 +0900

    rereresize

--- app/page.tsx
+++ app/page.tsx
@@ -385,10 +385,10 @@ export default function Home() {
                       <img src="/BI_kr.png" alt="BI 로고" className="max-h-12 max-w-full object-contain" />
                     </div>
                     <div className="flex items-center justify-center h-16 min-w-[120px] flex-shrink-0">
-                      <img src="/KOG-LOGO_PNG.png" alt="KOG 로고" className="max-h-14 max-w-full object-contain" />
+                      <img src="/KOG-LOGO_PNG.png" alt="KOG 로고" className="max-h-13 max-w-full object-contain" />
                     </div>
                     <div className="flex items-center justify-center h-16 min-w-[120px] flex-shrink-0">
-                      <img src="/KRAFTON_Black.png" alt="KRAFTON 로고" className="max-h-10 max-w-full object-contain" />
+                      <img src="/KRAFTON_Black.png" alt="KRAFTON 로고" className="max-h-8 max-w-full object-contain" />
                     </div>
                     <div className="flex items-center justify-center h-16 min-w-[120px] flex-shrink-0">
                       <img
@@ -418,7 +418,7 @@ export default function Home() {
                       <img
                         src="/Devsisters.png"  //  todo : should be modified to Devsisters logo
                         alt="Devsisters 로고"
-                        className="max-h-10 max-w-full object-contain"
+                        className="max-h-8 max-w-full object-contain"
                       />
                     </div>
 
@@ -427,10 +427,10 @@ export default function Home() {
                       <img src="/BI_kr.png" alt="BI 로고" className="max-h-12 max-w-full object-contain" />
                     </div>
                     <div className="flex items-center justify-center h-16 min-w-[120px] flex-shrink-0">
-                      <img src="/KOG-LOGO_PNG.png" alt="KOG 로고" className="max-h-14 max-w-full object-contain" />
+                      <img src="/KOG-LOGO_PNG.png" alt="KOG 로고" className="max-h-13 max-w-full object-contain" />
                     </div>
                     <div className="flex items-center justify-center h-16 min-w-[120px] flex-shrink-0">
-                      <img src="/KRAFTON_Black.png" alt="KRAFTON 로고" className="max-h-10 max-w-full object-contain" />
+                      <img src="/KRAFTON_Black.png" alt="KRAFTON 로고" className="max-h-8 max-w-full object-contain" />
                     </div>
                     <div className="flex items-center justify-center h-16 min-w-[120px] flex-shrink-0">
                       <img
@@ -460,7 +460,7 @@ export default function Home() {
                       <img
                         src="/Devsisters.png"  //  todo : should be modified to Devsisters logo
                         alt="Devsisters 로고"
-                        className="max-h-10 max-w-full object-contain"
+                        className="max-h-8 max-w-full object-contain"
                       />
                     </div>
                     
